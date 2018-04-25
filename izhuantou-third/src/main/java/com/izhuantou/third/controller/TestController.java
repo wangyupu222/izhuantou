36ed01aad63edc197eb0fb8d43fd34074c9a1b89
李宗跃 <zongyue.li@foriseholdings.com>
2018-04-26 00:05:33 +0800

    third项目支持web

---
+++ izhuantou-third/src/main/java/com/izhuantou/third/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.izhuantou.third.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.izhuantou.service.api.ServiceTest;
+
+@Controller
+@RequestMapping("demo")
+public class TestController {
+
+	@Autowired
+	private ServiceTest serviceTest;
+
+	@RequestMapping(value = "/print")
+	@ResponseBody
+	public String queryMenuByUserName() {
+		System.err.println("我在admin的controller中调用的Service为 " + serviceTest.getNumber());
+		return "goodBay";
+	}
+}
