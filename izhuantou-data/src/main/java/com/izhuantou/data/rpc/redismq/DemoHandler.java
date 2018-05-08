be563ff334964fdf96e005f68aedc67dbe482849
Administrator <Administrator@windows10.microdone.cn>
2018-05-08 10:24:39 +0800

    项目添加redisMQ

--- izhuantou-data/src/main/java/com/izhuantou/data/rpc/redismq/DemoHandler.java
+++ izhuantou-data/src/main/java/com/izhuantou/data/rpc/redismq/DemoHandler.java
@@ -5,41 +5,40 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import com.izhuantou.common.redismq.MessageType;
+import com.izhuantou.common.utils.JsonUtil;
+import com.izhuantou.damain.vo.bidding.BiddingDTO;
 import com.koomii.redismq.AbstarctMessageHandler;
-
 
 /**
  * DemoMessage消息的处理器
  * 
  * @category @author fucheng
- * @since 2018年5月8日 
+ * @since 2018年5月8日
  */
 @Service
-public class DemoHandler extends AbstarctMessageHandler
-{
-	private static Log logger = LogFactory.getLog(DemoHandler.class);
+public class DemoHandler extends AbstarctMessageHandler {
+    private static Log logger = LogFactory.getLog(DemoHandler.class);
 
-	public DemoHandler()
-	{
-		// 说明该handler监控的消息类型
-		super(MessageType.DEMO_MESSAGE, 10);
-	}
+    public DemoHandler() {
+	// 说明该handler监控的消息类型
+	super(MessageType.NOVICE_BIDDING_MESSAGE, 10);
+    }
 
-	/**
-	 * 监听到消息后处理方法
-	 */
-	@Override
-	public void handle(String message)
-	{
-		System.out.println("正在处理消息：" + message);
-	}
+    /**
+     * 监听到消息后处理方法
+     */
+    @Override
+    public void handle(String message) {
 
-	@Override
-	public void handleFailed(String obj)
-	{
-		StringBuilder sb = new StringBuilder();
-		sb.append("msg:[").append(obj).append("], 超过失败次数，停止重试。");
-		logger.warn(sb.toString());
-	}
+	BiddingDTO biddingDTO = JsonUtil.toObj(message, BiddingDTO.class);
+	System.out.println("获取到的message为：" + biddingDTO);
+    }
+
+    @Override
+    public void handleFailed(String obj) {
+	StringBuilder sb = new StringBuilder();
+	sb.append("msg:[").append(obj).append("], 超过失败次数，停止重试。");
+	logger.warn(sb.toString());
+    }
 
 }
--- izhuantou-fund/src/main/java/com/izhuantou/fund/rpc/impl/bidding/NoviceBiddingImpl.java
+++ izhuantou-fund/src/main/java/com/izhuantou/fund/rpc/impl/bidding/NoviceBiddingImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.dubbo.common.utils.StringUtils;
+import com.izhuantou.common.redismq.MessageType;
+import com.izhuantou.common.utils.JsonUtil;
 import com.izhuantou.damain.user.MemberMemberPrivilege;
 import com.izhuantou.damain.vo.bidding.BiddingDTO;
 import com.izhuantou.damain.webp2p.WebP2pLoanProductRateInfo;
@@ -21,6 +23,8 @@ import com.izhuantou.dao.webp2p.WebP2pProductRateInfoMapper;
 import com.izhuantou.fund.rpc.api.bidding.BiddingCashFreeze;
 import com.izhuantou.fund.rpc.api.bidding.NoviceBidding;
 import com.izhuantou.fund.rpc.impl.BaseServiceImpl;
+import com.koomii.redismq.Message;
+import com.koomii.redismq.RedisMQMessageSender;
 
 /**
  * 新手标的相关实现类
@@ -29,127 +33,166 @@ import com.izhuantou.fund.rpc.impl.BaseServiceImpl;
  * @date 2018-03-06
  */
 @Service("noviceBidding")
-public class NoviceBiddingImpl extends BaseServiceImpl<WebP2pNoviceBiddingRuning>
-		implements NoviceBidding {
-	private static final Logger logger = LoggerFactory.getLogger(NoviceBiddingImpl.class);
+public class NoviceBiddingImpl extends BaseServiceImpl<WebP2pNoviceBiddingRuning> implements NoviceBidding {
+    private static final Logger logger = LoggerFactory.getLogger(NoviceBiddingImpl.class);
 
-	@Autowired
-	private BiddingCashFreeze controlDebitCredit;
-	@Autowired
-	private MemberMemberPrivilegeMapper memberMemberPrivilegeDao;
-	@Autowired
-	private WebP2pNoviceBiddingRuningMapper noviceBiddingRuningMapper;
-	@Autowired
-	private WebP2pProductRateInfoMapper ProductRateInfoMapper;
-	@Autowired
-	private WebP2pLoanProductRateInfoMapper loanProductRateInfoMapper;
+    @Autowired
+    private BiddingCashFreeze controlDebitCredit;
+    @Autowired
+    private MemberMemberPrivilegeMapper memberMemberPrivilegeDao;
+    @Autowired
+    private WebP2pNoviceBiddingRuningMapper noviceBiddingRuningMapper;
+    @Autowired
+    private WebP2pProductRateInfoMapper ProductRateInfoMapper;
+    @Autowired
+    private WebP2pLoanProductRateInfoMapper loanProductRateInfoMapper;
+    @Autowired
+    private RedisMQMessageSender messageSender;
 
-	@Override
-	public String bidNoviceBidding(BiddingDTO biddto) {
-		try {
-			// 确认用户的输入金额有没有超过头笔赚出借上限
-			if (biddto != null) {
-				if (biddto.getAmount().compareTo(new BigDecimal(10000)) > 0) {
-					// 表示用户输入金额大于投标上限
-					logger.info("投标失败,出借金额超过头笔赚产品出借上限，请重新输入出借金额");
-					return null;
-				} else {
-					String result ="";
-					// 表示可以投标
-					WebP2pNoviceBiddingRuning noviceBid = this.noviceBiddingRuningMapper
-							.findByOID(biddto.getBiddingOID());
-					if (noviceBid != null) {
-						WebP2pProductRateInfo productRate = this.ProductRateInfoMapper
-								.findByOID(noviceBid.getProductRateInfoID());
-						WebP2pLoanProductRateInfo productRateInfo = this.loanProductRateInfoMapper
-								.findByOID(noviceBid.getLoanProductRateInfoID());
-						// 表示可以投标
-						if (noviceBid.getBiddingType().equals("0")) {
-							// 普通标的
-							if (StringUtils.isEmpty(biddto.getTqOID())) {
-								biddto.setCreditRate(productRateInfo.getYearRate());
-								biddto.setCreditType(productRate.getRepaymentType());
-								// 投标完成后需要期数有借款期数 biddto需要放入 redis
-								biddto.setTerm(productRateInfo.getTerm());
-								
-								result = this.controlDebitCredit.addDebitCreditNew(biddto);
-								return result;
-							} else {
-								MemberMemberPrivilege tq = memberMemberPrivilegeDao.findByOID(biddto.getTqOID());
-								if (tq != null) {
-									if ("0".equals(tq.getState())) {
-										BigDecimal privilegePrincipal = tq.getTqAmount();
-										BigDecimal privilegeInterest = tq.getTqll();
-										biddto.setCreditRate(productRateInfo.getYearRate());
-										biddto.setCreditType(productRate.getRepaymentType());
-										biddto.setPrivilegeInterest(privilegeInterest);
-										biddto.setPrivilegePrincipal(privilegePrincipal);
-										// 投标完成后需要期数有借款期数
-										biddto.setTerm(productRateInfo.getTerm());
-										
-										result = controlDebitCredit.addDebitCreditNew(biddto);
-										if(StringUtils.isNotEmpty(result)&&"1".equals(result)){
-											tq.setState("1");
-											tq.setBiddingName(biddto.getBiddingOID());
-											// 修改状态
-											memberMemberPrivilegeDao.updState(tq);
-										}
-									}
-								} else {
-									logger.info("特权不存在");
-								}
-							}
-							return result;
-						} else if (noviceBid.getBiddingType().equals("1")) {
-							if (StringUtils.isEmpty(biddto.getTqOID())) {
-								biddto.setDebitCreditOID(noviceBid.getDebitCreditOID());
-								biddto.setOutMemberOID(noviceBid.getMemberOID());
-								biddto.setBeginDate(noviceBid.getLoanDay());
-								biddto.setTransferReturnDate(new Date());
-								biddto.setIsTZorCW("XS");
-								biddto.setCreditRate(productRateInfo.getYearRate());
-								biddto.setCreditType(productRate.getRepaymentType());
-								result=controlDebitCredit.addTransferReturnZZ(biddto);
-							} else {
-								MemberMemberPrivilege tq = memberMemberPrivilegeDao.findByOID(biddto.getTqOID());
-								if (tq != null) {
-									if ("0".equals(tq.getTqOID())) {
-										BigDecimal privilegePrincipal = tq.getTqAmount();
-										BigDecimal privilegeInterest = tq.getTqll();
-										biddto.setCreditRate(productRateInfo.getYearRate());
-										biddto.setCreditType(productRate.getRepaymentType());
-										biddto.setPrivilegePrincipal(privilegePrincipal);
-										biddto.setPrivilegeInterest(privilegeInterest);
-										biddto.setDebitCreditOID(noviceBid.getDebitCreditOID());
-										biddto.setOutMemberOID(biddto.getMemberOID());
-										biddto.setTransferReturnDate(new Date());
-										result=controlDebitCredit.addTransferReturn(biddto);
-										if(StringUtils.isNotEmpty(result)&&"1".equals(result)){
-											tq.setState("1");
-											tq.setBiddingName(biddto.getBiddingOID());
-											// 修改状态
-											memberMemberPrivilegeDao.updState(tq);
-										}
-									}
-								}
-							}			
-							return result;
-						} else {
-							logger.info("======对应标的类型不匹配========");
-						}
-					}
+    @Override
+    public String bidNoviceBidding(BiddingDTO biddto) {
+	try {
+	    // 确认用户的输入金额有没有超过头笔赚出借上限
+	    if (biddto != null) {
+		if (biddto.getAmount().compareTo(new BigDecimal(10000)) > 0) {
+		    // 表示用户输入金额大于投标上限
+		    logger.info("投标失败,出借金额超过头笔赚产品出借上限，请重新输入出借金额");
+		    return null;
+		} else {
+		    String result = "";
+		    // 表示可以投标
+		    WebP2pNoviceBiddingRuning noviceBid = this.noviceBiddingRuningMapper
+			    .findByOID(biddto.getBiddingOID());
+		    if (noviceBid != null) {
+			WebP2pProductRateInfo productRate = this.ProductRateInfoMapper
+				.findByOID(noviceBid.getProductRateInfoID());
+			WebP2pLoanProductRateInfo productRateInfo = this.loanProductRateInfoMapper
+				.findByOID(noviceBid.getLoanProductRateInfoID());
+			// 表示可以投标
+			if (noviceBid.getBiddingType().equals("0")) {
+			    // 普通标的
+			    if (StringUtils.isEmpty(biddto.getTqOID())) {
+				biddto.setCreditRate(productRateInfo.getYearRate());
+				biddto.setCreditType(productRate.getRepaymentType());
+				// 投标完成后需要期数有借款期数 biddto需要放入 redis
+				biddto.setTerm(productRateInfo.getTerm());
+
+				result = this.controlDebitCredit.addDebitCreditNew(biddto);
+				if (StringUtils.isNotEmpty(result) && "1".equals(result)) {
+				    // 将新手原标数据放到redis队列中,没有特权
+				    String biddtoStr = JsonUtil.toStr(biddto);
+				    Long mqResult = messageSender
+					    .put(new Message(MessageType.NOVICE_BIDDING_MESSAGE, biddtoStr));
+				    if (mqResult != null) {
+					System.out.println("消息发送成功");
+				    } else {
+					System.out.println("消息发送失败");
+				    }
 				}
-			}
-			logger.info("======投标失败=======");
-			return null;
-		} catch (Exception e) {
-			logger.error("bidNoviceBidding(BiddingDTO biddto)" + e.getMessage());
-			return null;
-		}
+				return result;
+			    } else {
+				MemberMemberPrivilege tq = memberMemberPrivilegeDao.findByOID(biddto.getTqOID());
+				if (tq != null) {
+				    if ("0".equals(tq.getState())) {
+					BigDecimal privilegePrincipal = tq.getTqAmount();
+					BigDecimal privilegeInterest = tq.getTqll();
+					biddto.setCreditRate(productRateInfo.getYearRate());
+					biddto.setCreditType(productRate.getRepaymentType());
+					biddto.setPrivilegeInterest(privilegeInterest);
+					biddto.setPrivilegePrincipal(privilegePrincipal);
+					// 投标完成后需要期数有借款期数
+					biddto.setTerm(productRateInfo.getTerm());
 
+					result = controlDebitCredit.addDebitCreditNew(biddto);
+					if (StringUtils.isNotEmpty(result) && "1".equals(result)) {
+					    // 将新手原标数据放到redis队列中,有特权
+					    String biddtoStr = JsonUtil.toStr(biddto);
+					    Long mqResult = messageSender
+						    .put(new Message(MessageType.NOVICE_BIDDING_MESSAGE, biddtoStr));
+					    if (mqResult != null) {
+						System.out.println("消息发送成功");
+					    } else {
+						System.out.println("消息发送失败");
+					    }
+					    tq.setState("1");
+					    tq.setBiddingName(biddto.getBiddingOID());
+					    // 修改状态
+					    memberMemberPrivilegeDao.updState(tq);
+
+					}
+				    }
+				} else {
+				    logger.info("特权不存在");
+				}
+			    }
+			    return result;
+			} else if (noviceBid.getBiddingType().equals("1")) {
+			    if (StringUtils.isEmpty(biddto.getTqOID())) {
+				biddto.setDebitCreditOID(noviceBid.getDebitCreditOID());
+				biddto.setOutMemberOID(noviceBid.getMemberOID());
+				biddto.setBeginDate(noviceBid.getLoanDay());
+				biddto.setTransferReturnDate(new Date());
+				biddto.setIsTZorCW("XS");
+				biddto.setCreditRate(productRateInfo.getYearRate());
+				biddto.setCreditType(productRate.getRepaymentType());
+				result = controlDebitCredit.addTransferReturnZZ(biddto);
+				if (StringUtils.isNotEmpty(result) && "1".equals(result)) {
+				    // 将新手债转标数据放到redis队列中，没有特权
+				    String biddtoStr = JsonUtil.toStr(biddto);
+				    Long mqResult = messageSender
+					    .put(new Message(MessageType.NOVICE_TRANSFER_BIDDING_MESSAGE, biddtoStr));
+				    if (mqResult != null) {
+					System.out.println("消息发送成功");
+				    } else {
+					System.out.println("消息发送失败");
+				    }
+				}
+			    } else {
+				MemberMemberPrivilege tq = memberMemberPrivilegeDao.findByOID(biddto.getTqOID());
+				if (tq != null) {
+				    if ("0".equals(tq.getTqOID())) {
+					BigDecimal privilegePrincipal = tq.getTqAmount();
+					BigDecimal privilegeInterest = tq.getTqll();
+					biddto.setCreditRate(productRateInfo.getYearRate());
+					biddto.setCreditType(productRate.getRepaymentType());
+					biddto.setPrivilegePrincipal(privilegePrincipal);
+					biddto.setPrivilegeInterest(privilegeInterest);
+					biddto.setDebitCreditOID(noviceBid.getDebitCreditOID());
+					biddto.setOutMemberOID(biddto.getMemberOID());
+					biddto.setTransferReturnDate(new Date());
+					result = controlDebitCredit.addTransferReturn(biddto);
+					if (StringUtils.isNotEmpty(result) && "1".equals(result)) {
+					    // 将新手债转标数据放到redis队列中，有特权
+					    String biddtoStr = JsonUtil.toStr(biddto);
+					    Long mqResult = messageSender.put(new Message(
+						    MessageType.NOVICE_TRANSFER_BIDDING_MESSAGE, biddtoStr));
+					    if (mqResult != null) {
+						System.out.println("消息发送成功");
+					    } else {
+						System.out.println("消息发送失败");
+					    }
+					    tq.setState("1");
+					    tq.setBiddingName(biddto.getBiddingOID());
+					    // 修改状态
+					    memberMemberPrivilegeDao.updState(tq);
+					}
+				    }
+				}
+			    }
+			    return result;
+			} else {
+			    logger.info("======对应标的类型不匹配========");
+			}
+		    }
+		}
+	    }
+	    logger.info("======投标失败=======");
+	    return null;
+	} catch (Exception e) {
+	    logger.error("bidNoviceBidding(BiddingDTO biddto)" + e.getMessage());
+	    return null;
 	}
 
-	
-	
-	
+    }
+
 }
